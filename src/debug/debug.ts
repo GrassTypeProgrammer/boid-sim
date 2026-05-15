2f338b26f84fd9c945c86498501ea07f7ecefe4a
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2026-05-15 06:07:47 +0100

    Improve debug layout

--- src/debug/debug.ts
+++ src/debug/debug.ts
@@ -6,19 +6,35 @@ let gui: GUI | null = null;
 export function getGUI() {
   if (!gui) {
     gui = new GUI();
-    gui.add(worldValues, 'turnFactor', 0, 1);
-    gui.add(worldValues, 'borderMargin', 1, 100);
+    const boidFolder = gui.addFolder('Boids');
+    const borderFolder = boidFolder.addFolder('Borders');
+    // TODO: Rename turnfactor and borderMargin
+    borderFolder.add(worldValues, 'turnFactor', 0, 1);
+    borderFolder.add(worldValues, 'borderMargin', 1, 100);
 
-    gui.add(worldValues, 'separationDistance', 0, 200);
-    gui.add(worldValues, 'neighbourDistance', 0, 200);
+    const distanceFolder = boidFolder.addFolder('Distances');
+    distanceFolder
+      .add(worldValues, 'separationDistance', 0, 200)
+      .name('Separation');
+    distanceFolder
+      .add(worldValues, 'neighbourDistance', 0, 200)
+      .name('Neighbour');
 
-    gui.add(worldValues, 'cohesionStrength', 0, 0.5);
-    gui.add(worldValues, 'boidAvoidanceStrength', 0, 1);
-    gui.add(worldValues, 'alignmentStrength', 0, 0.5);
+    const strengthFolder = boidFolder.addFolder('Strengths');
+    strengthFolder
+      .add(worldValues, 'cohesionStrength', 0, 0.5)
+      .name('Cohesion');
+    strengthFolder
+      .add(worldValues, 'boidAvoidanceStrength', 0, 1)
+      .name('Boid Avoidance');
+    strengthFolder
+      .add(worldValues, 'alignmentStrength', 0, 0.5)
+      .name('Alignment');
 
-    gui.add(debugValues, 'showNeighbours');
-    gui.add(debugValues, 'pause');
-    gui.add(debugValues, 'neighbourRadius');
+    const debugGui: GUI = gui.addFolder('Debug');
+    debugGui.add(debugValues, 'showNeighbours');
+    debugGui.add(debugValues, 'pause');
+    debugGui.add(debugValues, 'neighbourRadius');
   }
 
   return gui;
